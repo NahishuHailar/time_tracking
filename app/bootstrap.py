e3066c49a9ce11a1e72a3ffaa077b247218f2dce
Udalykh_Konstantin <k.n.udalykh@gmail.com>
2025-03-06 10:30:14 +0300

    Changed bootstrap.py

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI
-from api.v1.endpoits import project, user,  project_user, time_entry
-from exceptions import AppError
-from middleware.error_handler import app_error_handler
 
 class AppFactory:
 
     @staticmethod
     def create_app() -> FastAPI:
         app = FastAPI()
-
-        app.include_router(project.router, prefix="/api/v1")
-        app.include_router(user.router, prefix="/api/v1")
-        app.include_router(time_entry.router, prefix="/api/v1")
-        app.include_router(project_user.router, prefix="/api/v1")
-
-        app.add_exception_handler(AppError, app_error_handler)
-
         return app
--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 from bootstrap import AppFactory
+from api.v1.endpoits import project, user,  project_user, time_entry
+from exceptions import AppError
+from middleware.error_handler import app_error_handler
 
 app = AppFactory.create_app()
+
+app.include_router(project.router, prefix="/api/v1")
+app.include_router(user.router, prefix="/api/v1")
+app.include_router(time_entry.router, prefix="/api/v1")
+app.include_router(project_user.router, prefix="/api/v1")
+
+app.add_exception_handler(AppError, app_error_handler)       
