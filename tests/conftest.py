dfc6c29221475eee35193a72beb20447c8faa3a0
Udalykh_Konstantin <k.n.udalykh@gmail.com>
2025-03-09 15:09:59 +0300

    1) conftest.py: added event_loop fixture, changed client fixture(override dependesy database.get_db for test db connection) 2) Added integration tests for project api functions: update_project, read_project, delete_project

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import pytest_asyncio
+import asyncio
 
 import os
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -18,15 +19,29 @@ def apply_migrations():
     alembic_cfg = Config("alembic.ini")
     command.upgrade(alembic_cfg, "head")
 
-@pytest_asyncio.fixture
-async def client() -> AsyncClient:
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://localhost:8000") as ac:
-        yield ac
 
 @pytest_asyncio.fixture
 async def test_db() -> AsyncSession:
     async for session in database.get_db():
+        await session.begin() 
         yield session
         await session.rollback()
 
 
+@pytest_asyncio.fixture
+async def client(test_db) -> AsyncClient:
+    async def override_get_db():
+        yield test_db
+
+    app.dependency_overrides[database.get_db] = override_get_db
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://localhost:8000") as ac:
+        yield ac
+    app.dependency_overrides.clear()    
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
--- tests/integration/endpoints/test_project_api.py
+++ tests/integration/endpoints/test_project_api.py
@@ -1,50 +1,50 @@
 import pytest
+from app.db.models.project import ProjectORM
 
 
 @pytest.mark.asyncio
 async def test_create_project(client, test_db):
-    print(f"Type of test_db: {type(test_db)}")
     response = await client.post("/api/v1/projects/", json={"name": "New Project"})
     assert response.status_code == 200
     data = response.json()
     assert data["name"] == "New Project"
     assert "id" in data
 
-# @pytest.mark.asyncio
-# async def test_get_project(client, test_db):
-#     print(f"Type of test_db: {type(test_db)}")
-#     project = ProjectORM(id=1, name="Test Project")
-#     test_db.add(project)
-#     await test_db.flush()
+@pytest.mark.asyncio
+async def test_get_project(client, test_db):
+    project = ProjectORM(name="Test Project")
+    test_db.add(project)
+    await test_db.flush()
+    await test_db.refresh(project)
 
-#     response = await client.get(f"/api/v1/projects/{project.id}")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert data["id"] == project.id
-#     assert data["name"] == project.name
+    response = await client.get(f"/api/v1/projects/{project.id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == project.id
+    assert data["name"] == project.name
 
-# @pytest.mark.asyncio
-# async def test_update_project(client, test_db):
-#     print(f"Type of test_db: {type(test_db)}")
-#     project = ProjectORM(id=2, name="Old Name")
-#     test_db.add(project)
-#     await test_db.flush()
+@pytest.mark.asyncio
+async def test_update_project(client, test_db):
+    project = ProjectORM(name="Old Name")
+    test_db.add(project)
+    await test_db.flush()
+    await test_db.refresh(project)
 
-#     response = await client.put(f"/api/v1/projects/{project.id}", json={"name": "Updated Name"})
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert data["name"] == "Updated Name"
+    response = await client.put(f"/api/v1/projects/{project.id}", json={"name": "Updated Name"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "Updated Name"
 
-# @pytest.mark.asyncio
-# async def test_delete_project(client, test_db):
-#     print(f"Type of test_db: {type(test_db)}")
-#     project = ProjectORM(id=3, name="To Delete")
-#     test_db.add(project)
-#     await test_db.flush()
+@pytest.mark.asyncio
+async def test_delete_project(client, test_db):
+    project = ProjectORM(name="To Delete")
+    test_db.add(project)
+    await test_db.flush()
+    await test_db.refresh(project)
 
-#     response = await client.delete(f"/api/v1/projects/{project.id}")
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Project deleted"}
+    response = await client.delete(f"/api/v1/projects/{project.id}")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Project deleted"}
 
-#     response = await client.get(f"/api/v1/projects/{project.id}")
-#     assert response.status_code == 404  
+    response = await client.get(f"/api/v1/projects/{project.id}")
+    assert response.status_code == 404  
