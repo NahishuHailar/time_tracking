716c8827ff967b6fa351f85f982c4a7090964578
Udalykh_Konstantin <k.n.udalykh@gmail.com>
2025-03-15 19:10:12 +0300

    fixed 5 ci.yml

--- tests/const.py
+++ tests/const.py
@@ -3,7 +3,7 @@ test_db_sett = {
     'postgres_password': '4721',
     'postgres_db': 'test_db',
     'db_host': 'localhost',
-    'db_port': 'postgres',
+    'db_port': '5432',
     'db_echo': False
 }
 
